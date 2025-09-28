99cb087ba0ca43f99dd867ba2921a526689fa754
Nikita Skobelevs <nikita.skobelevs@gmail.com>
2025-09-28 12:03:16 +0300

    Skeleton out web server

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,47 @@
-use std::fs;
+use actix_web::{App, HttpResponse, HttpServer, Responder, delete, get, post, put, web};
 
-use evaluator::core::rule::Rule;
+#[get("/rules")]
+async fn get_all_rules_handler() -> impl Responder {
+    HttpResponse::Ok().body("todo")
+}
 
-fn main() {
-    let input = fs::read_to_string("rules.json").unwrap();
+#[get("/rules/{id}")]
+async fn get_rule_handler(id: web::Path<String>) -> impl Responder {
+    HttpResponse::Ok().body("todo")
+}
 
-    let rules: Vec<Rule> = serde_json::from_str(&input).unwrap();
+#[post("/rules")]
+async fn create_rule_handler() -> impl Responder {
+    HttpResponse::Ok().body("todo")
+}
 
-    println!("{rules:?}");
+#[delete("/rules/{id}")]
+async fn delete_rule_handler(id: web::Path<String>) -> impl Responder {
+    HttpResponse::Ok().body("todo")
+}
+
+#[put("/rules/{id}")]
+async fn update_rule_handler(id: web::Path<String>) -> impl Responder {
+    HttpResponse::Ok().body("todo")
+}
+
+#[post("/evaluate")]
+async fn evaluate_rules_handler() -> impl Responder {
+    HttpResponse::Ok().body("todo")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(get_all_rules_handler)
+            .service(get_rule_handler)
+            .service(create_rule_handler)
+            .service(delete_rule_handler)
+            .service(update_rule_handler)
+            .service(evaluate_rules_handler)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
